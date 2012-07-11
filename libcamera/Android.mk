2c27b5dce82353b16c0a79ff1d9ff9d9d1521953
Ricardo Soares <ricjoi@hotmail.com>
2012-07-11 12:52:01 +0100

    Missing files

---
+++ libcamera/Android.mk
@@ -0,0 +1,27 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_C_FLAGS          += -O3
+LOCAL_MODULE_TAGS      := optional
+LOCAL_MODULE_PATH      := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE           := camera.$(TARGET_BOOTLOADER_BOARD_NAME)
+
+LOCAL_SRC_FILES        := QcomCamera.cpp
+
+LOCAL_SHARED_LIBRARIES := liblog libdl libutils libcamera_client libbinder \
+                          libcutils libhardware libui libcamera
+
+LOCAL_C_INCLUDES       := frameworks/base/services \
+                          frameworks/base/include \
+                          hardware/libhardware/include
+ifeq ($(TARGET_BOARD_PLATFORM),msm7k)
+LOCAL_C_INCLUDES       += hardware/libhardware/modules/gralloc
+LOCAL_CFLAGS           := -DPREVIEW_MSM7K
+else
+LOCAL_C_INCLUDES       += hardware/qcom/display/libgralloc
+endif
+
+LOCAL_PRELINK_MODULE   := false
+
+include $(BUILD_SHARED_LIBRARY)
